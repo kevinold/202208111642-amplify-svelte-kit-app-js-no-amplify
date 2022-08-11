5b6c228e4d948b7376b670601cfabf2d37a0b184
Kevin Old <kevin@kevinold.com>
2022-08-11 16:47:51 -0500

    amplify init and graphql api

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "20220811sveltessr": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
