54ce41ac577eb18c23a8491b4defa34e8a5d5387
Kevin Old <kevin@kevinold.com>
2022-08-12 09:38:32 -0500

    render todos in ssr

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,4 @@
-<script>
+<script context="module">
     import { Amplify,API } from "aws-amplify";
     import awsExports from '../aws-exports';
     import { listTodos } from "../graphql/queries";
@@ -9,20 +9,33 @@
 
         return {
             props: {
-                todos: data.listPosts.items
+                todos: data?.listTodos?.items
             }
         }
     }
+</script>
+<script>
     export let todos
 </script>
 
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<h2>Todos</h2>
 <div>
     {#each todos as todo (todo.id)}
-		<div class="todo">
-            {todo.id}
-        </div>
+		<table class="todo">
+          <tr>
+            <th>ID</th> 
+            <th>Name</th> 
+            <th>Description</th> 
+          </tr>
+          <tr>
+            <td>{todo.id}</td> 
+            <td>{todo.name}</td> 
+            <td>{todo.description}</td> 
+          </tr>
+        </table>
     {/each}
 </div>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
