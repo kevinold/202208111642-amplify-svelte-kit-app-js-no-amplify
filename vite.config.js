d7d95ab7203eb397825d0dfd3aa0f3c597368caf
Kevin Old <kevin@kevinold.com>
2022-08-12 09:44:06 -0500

    add workaround to vite.config.js to get production build

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	// per https://github.com/aws-amplify/amplify-js/issues/9639#issuecomment-1081781840
+	resolve: { alias: { './runtimeConfig': './runtimeConfig.browser' } }
 };
 
 export default config;
