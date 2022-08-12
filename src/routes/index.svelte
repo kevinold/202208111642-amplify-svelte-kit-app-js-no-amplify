138d727654dcf458399a7b72de56ed192d7317a6
Kevin Old <kevin@kevinold.com>
2022-08-12 09:24:58 -0500

    generate amplify graphql api

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,28 @@
+<script>
+    import { Amplify,API } from "aws-amplify";
+    import awsExports from '../aws-exports';
+    import { listTodos } from "../graphql/queries";
+
+    export async function load() {
+	    Amplify.configure({ ...awsExports, ssr: true });
+        const { data } = await API.graphql({ query: listTodos })
+
+        return {
+            props: {
+                todos: data.listPosts.items
+            }
+        }
+    }
+    export let todos
+</script>
+
+<div>
+    {#each todos as todo (todo.id)}
+		<div class="todo">
+            {todo.id}
+        </div>
+    {/each}
+</div>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
